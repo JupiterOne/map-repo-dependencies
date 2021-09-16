a8bd88f03f24cae000f0b7c9ce774dea60476bf7
Ben Johnson <ben.johnson@jupiterone.com>
2021-09-16 10:29:07 -0400

    Add initial query to retrieve accessrole/accesspolicy allows relationships

--- src/get-client.ts
+++ src/get-client.ts
@@ -1,19 +1,21 @@
-const JupiterOneClient = require('@jupiterone/jupiterone-client-nodejs');
+import { JupiterOneClient } from "@jupiterone/jupiterone-client-nodejs";
 
-export async function getClient(clientInput: {account, accessToken}) {
-  const account = clientInput.account === '' ?
-    process.env.J1_ACCOUNT :
-    clientInput.account;
-  const accessToken = clientInput.accessToken === '' ? 
-    process.env.J1_API_TOKEN :
-    clientInput.accessToken;
+// const JupiterOneClient = require('@jupiterone/jupiterone-client-nodejs');
+
+export async function getClient(clientInput: { account; accessToken }) {
+  const account =
+    clientInput.account === "" ? process.env.J1_ACCOUNT : clientInput.account;
+  const accessToken =
+    clientInput.accessToken === ""
+      ? process.env.J1_API_TOKEN
+      : clientInput.accessToken;
   if (accessToken === undefined || account === undefined) {
-    throw(console.error('ERROR: MISSING CREDENTIALS'));
+    throw console.error("ERROR: MISSING CREDENTIALS");
   }
   const j1Client = await new JupiterOneClient({
     account,
     accessToken,
-    dev: process.env.J1_DEV || false
+    dev: !!process.env.J1_DEV,
   }).init();
 
   return j1Client;
--- src/index.ts
+++ src/index.ts
@@ -1,73 +1,21 @@
-import { getDependencies, getDeployDependencies } from './get-dependencies';
-import { getRepoIds } from './get-repos';
-import { getClient } from './get-client';
-import { question } from 'readline-sync';
-import { createRepoRelationships } from './create-repo-relationship';
+import { getClient } from "./get-client";
 
-require('dotenv').config();
-
-let success = 0, failure = 0;
-let missingDeps = [];
-let packageScope = [];
+require("dotenv").config();
 
 (async () => {
-  while (true) {
-    const scope = question(
-      '\nInput a package scope, e.g. @jupiterone (input ALL for everything, DONE when finished, HELP for other options):\n'
-    );
-    if (scope === 'DONE') {
-      break;
-    }
-    if (scope === 'ALL') {
-      packageScope.push(scope);
-      break;
-    }
-    if (scope === 'HELP') {
-      console.log('1. ALL = All dependencies');
-      console.log('2. (No Input) = All dependencies not starting with \'@\'');
-      continue;
-    }
-    packageScope.push(scope);
-  }
-  const pathToRepos = question('\nInput path to directory with repos (relative to root directory):\n');
-  const account = question('\nInput your JupiterOne account id. If using a .env file, continue without input:\n');
-  const access = question('\nInput your JupiterOne access token. If using a .env file, continue without input:\n');
-  const clientInput = {account: account, accessToken: access};
+  const j1Client = await getClient({
+    accessToken: process.env.J1_API_TOKEN,
+    account: process.env.J1_ACCOUNT,
+  });
 
-  console.log('');
-  const j1Client = await getClient(clientInput);
-  const repoMap = await getRepoIds(pathToRepos, clientInput);
-  console.log('');
+  const result = await j1Client.queryV1(`
+  Find (Function|Task) with displayName = 'jupiter-query-service' as f1 
+  THAT ASSIGNED AccessRole 
+  THAT ASSIGNED AccessPolicy 
+  THAT ALLOWS (Function|Task|DataStore) as f2
+  RETURN f1.displayName as source, f2.displayName as sink`);
 
-  for (const repoName of Array.from(repoMap.keys())) {
-    console.log('Repo: ' + repoName);
-
-    const mainRepo = await j1Client.queryV1(
-      `FIND CodeRepo WITH name='${repoName}'`
-    );
-    const depsList = getDependencies(repoName, packageScope, repoMap);
-    const deployDepsList = getDeployDependencies(repoName, repoMap);
-
-    if (depsList) {
-      const results = await createRepoRelationships(depsList, mainRepo, j1Client, repoName, missingDeps);
-      success += results.success;
-      failure += results.failure;
-    }
-    if (deployDepsList) {
-      const results = await createRepoRelationships(deployDepsList, mainRepo, j1Client, repoName, missingDeps, true);
-      success += results.success;
-      failure += results.failure;
-    }
-    
-    console.log('');
-  }
-  console.log('Summary:');
-  console.log('Created Relationships: ' + success);
-  console.log('Failed Attempts: ' + failure);
-  if (missingDeps.length > 0) {
-    console.log('Unable to create relationship to the following dependencies:');
-    console.log('  - ' + missingDeps.join('\n  - '));
-  }
-})().catch(err => {
-  console.error('', err);
+  console.log(result);
+})().catch((err) => {
+  console.error("", err);
 });
